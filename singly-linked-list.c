f27eb2f447daab8594de5755a4b85cdc92a66199
sangholee990 <80809068+sangholee990@users.noreply.github.com>
2021-04-13 00:34:33 +0900

    Create singly-linked-list.c

---
+++ singly-linked-list.c
@@ -0,0 +1,197 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+typedef struct Node {
+	int key;
+	struct Node* link;
+} listNode;
+
+typedef struct Head {
+	struct Node* first;
+}headNode;
+
+
+
+headNode* initialize(headNode* h);
+int freeList(headNode* h);
+
+int insertFirst(headNode* h, int key);
+int insertNode(headNode* h, int key);
+int insertLast(headNode* h, int key);
+
+int deleteFirst(headNode* h);
+int deleteNode(headNode* h, int key);
+int deleteLast(headNode* h);
+int invertList(headNode* h);
+
+void printList(headNode* h);
+
+int main()
+{
+	char command;
+	int key;
+	headNode* headnode=NULL;
+
+	do{
+		printf("----------------------------------------------------------------\n");
+		printf("                     Singly Linked List                         \n");
+		printf("----------------------------------------------------------------\n");
+		printf(" Initialize    = z           Print         = p \n");
+		printf(" Insert Node   = i           Delete Node   = d \n");
+		printf(" Insert Last   = n           Delete Last   = e\n");
+		printf(" Insert First  = f           Delete First  = t\n");
+		printf(" Invert List   = r           Quit          = q\n");
+		printf("----------------------------------------------------------------\n");
+
+		printf("Command = ");
+		scanf(" %c", &command);
+
+		switch(command) {
+		case 'z': case 'Z':
+			headnode = initialize(headnode);
+			break;
+		case 'p': case 'P':
+			printList(headnode);
+			break;
+		case 'i': case 'I':
+			printf("Your Key = ");
+			scanf("%d", &key);
+			insertNode(headnode, key);
+			break;
+		case 'd': case 'D':
+			printf("Your Key = ");
+			scanf("%d", &key);
+			deleteNode(headnode, key);
+			break;
+		case 'n': case 'N':
+			printf("Your Key = ");
+			scanf("%d", &key);
+			insertLast(headnode, key);
+			break;
+		case 'e': case 'E':
+			deleteLast(headnode);
+			break;
+		case 'f': case 'F':
+			printf("Your Key = ");
+			scanf("%d", &key);
+			insertFirst(headnode, key);
+			break;
+		case 't': case 'T':
+			deleteFirst(headnode);
+			break;
+		case 'r': case 'R':
+			invertList(headnode);
+			break;
+		case 'q': case 'Q':
+			freeList(headnode);
+			break;
+		default:
+			printf("\n       >>>>>   Concentration!!   <<<<<     \n");
+			break;
+		}
+
+	}while(command != 'q' && command != 'Q');
+
+	return 1;
+}
+
+headNode* initialize(headNode* h) {
+
+	if(h != NULL)
+		freeList(h);
+
+	headNode* temp = (headNode*)malloc(sizeof(headNode));
+	temp->first = NULL;
+	return temp;
+}
+
+int freeList(headNode* h){
+	
+	listNode* p = h->first;
+
+	listNode* prev = NULL;
+	while(p != NULL) {
+		prev = p;
+		p = p->link;
+		free(prev);
+	}
+	free(h);
+	return 0;
+}
+
+
+
+
+int insertFirst(headNode* h, int key) {
+
+	listNode* node = (listNode*)malloc(sizeof(listNode));
+	node->key = key;
+
+	node->link = h->first;
+	h->first = node;
+
+	return 0;
+}
+
+
+int insertNode(headNode* h, int key) {
+
+	return 0;
+}
+
+int insertLast(headNode* h, int key) {
+
+	return 0;
+}
+
+
+
+int deleteFirst(headNode* h) {
+
+
+	return 0;
+}
+
+
+int deleteNode(headNode* h, int key) {
+
+	return 0;
+
+}
+
+
+int deleteLast(headNode* h) {
+
+	return 0;
+}
+
+
+int invertList(headNode* h) {
+
+	return 0;
+}
+
+
+void printList(headNode* h) {
+	int i = 0;
+	listNode* p;
+
+	printf("\n---PRINT\n");
+
+	if(h == NULL) {
+		printf("Nothing to print....\n");
+		return;
+	}
+
+	p = h->first;
+
+	while(p != NULL) {
+		printf("[ [%d]=%d ] ", i, p->key);
+		p = p->link;
+		i++;
+	}
+
+	printf("  items = %d\n", i);
+}
+
